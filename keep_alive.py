eb196c8cda0c5ccc5643a1c6b61f413b2f06134d
succsuccsucc <109941602+succsuccsucc@users.noreply.github.com>
2023-01-06 00:51:16 +0800

    fixed keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,23 +1,15 @@
 from flask import Flask
 from threading import Thread
-import random
-
 
 app = Flask('')
 
 @app.route('/')
 def home():
-	return 'Im in!'
+    return "Hello. I am alive!"
 
 def run():
-  app.run(
-		host='0.0.0.0',
-		port=random.randint(2000,9000)
-	)
+  app.run(host='0.0.0.0',port=8080)
 
 def keep_alive():
-	'''
-	Creates and starts new thread that runs the function run.
-	'''
-	t = Thread(target=run)
-	t.start()
+    t = Thread(target=run)
+    t.start()
